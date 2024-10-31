79151119927e3679702146cc44887dce5038e743
M Salman Saleem <msalmansaleem1998@gmail.com>
2024-10-31 09:13:35 +0500

    header changes

--- src/component/header/header.js
+++ src/component/header/header.js
@@ -34,7 +34,7 @@ export default function Header() {
                 </g>{" "}
               </svg>
             </Link>
-            {/* Add Open class to toggle humburger menu */}
+            {/* Add Open class to toggle humburger/cross button */}
             <div className="btn-burger">
               <span />
               <span />
